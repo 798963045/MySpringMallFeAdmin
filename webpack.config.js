05b97ce04ba09e024004c997d08c3b74e7a0e6c9
KobePeng <798963045hao@gmail.com>
2017-09-14 23:25:18 +0800

    fina

--- webpack.config.js
+++ webpack.config.js
@@ -26,7 +26,7 @@ var config = {
     // path && publickPath
     output: {
         path        : __dirname + '/dist/',
-        publicPath  : WEBPACK_ENV === 'online' ? '//s.hpengsmile.com/admin-fe/dist/' : '/dist/',
+        publicPath  : WEBPACK_ENV === 'online' ? '//s.hpengsmile.cc/admin-fe/dist/' : '/dist/',
         filename    : 'js/[name].js'
     },
     resolve: {
